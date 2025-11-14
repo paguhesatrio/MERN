fe16c8e979cc789d09c9cff5b51ee8a798a62cda
Paguhesatrio <paguh2015@gmail.com>
2025-11-14 09:29:48 +0700

    first clean commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import router from "./routes/api";
+import db from "./utils/database";
+
+async function init() {
+  try {
+    const result = await db();
+    console.log("database status: ", result);
+
+    const app = express();
+    app.use(bodyParser.json());
+
+    const PORT = 4000;
+    app.use("/api", router);
+
+    app.listen(PORT, () => {
+      console.log(`running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
